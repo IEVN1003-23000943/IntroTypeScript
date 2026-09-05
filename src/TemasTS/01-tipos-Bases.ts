1dac0d5ddddbb95e8e1d55fb5827f5264e716544
IEVN1003-23000943 <82707@alumnos.utleon.edu.mx>
2026-09-04 19:27:45 -0600

    tipos de datos 04/09/2026

---
+++ src/TemasTS/01-tipos-Bases.ts
@@ -0,0 +1,16 @@
+console.log("Hola Mundo");
+
+var num1 = 23;
+let num2 = 65;
+const num3 = 22;
+
+let nombre:string = "pedro";
+let num:number;
+let activo:boolean;
+let sum:number = num1 + num2 + num3;
+console.log(sum);
+
+//let nombre = "pedro";
+console.log(nombre);
+console.log(nombre);
+console.log("Prueba1");
